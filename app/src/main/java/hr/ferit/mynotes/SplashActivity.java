4c72f700163cc2e4e055fe0f8b8240d13e86fef4
mariopetricevic <mario.petricevic22@gmail.com>
2020-07-08 11:02:24 +0200

    New app update

--- app/src/main/java/hr/ferit/mynotes/SplashActivity.java
+++ app/src/main/java/hr/ferit/mynotes/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        //for passwd save
         SharedPreferences settings=getSharedPreferences("PREPS", 0 );
         password=settings.getString("password", "");
 
